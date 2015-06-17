9f3e5c5621427bb4c96ddf21e08632e897e7459e
Maxwell Bloch <mbloch@connectionseducation.com>
2015-06-17 16:49:00 -0400

    (RED) Added test Underscore_exists

---
+++ UnderscoreCs/UnderscoreCs.Tests/Underscore.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnderscoreCs;
+
+namespace UnderscoreCs.Tests {
+	[TestClass]
+	public class Underscore {
+		
+		[TestMethod]
+		public async Task Underscore_exists() {
+			PrivateType type = new PrivateType("UnderscoreCs", "UnderscoreCs._");
+		}
+	}
+}
