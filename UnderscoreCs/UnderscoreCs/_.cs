3a04e351fb8309e6dc82e4df59ddc4babab1cd93
Maxwell Bloch <mbloch@connectionseducation.com>
2015-06-17 17:13:44 -0400

    (GREEN) Each_GivenIEnumerable_AndIteratee_WhenCalled_ThenEachElementInListIsVisited

--- UnderscoreCs/UnderscoreCs/_.cs
+++ UnderscoreCs/UnderscoreCs/_.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 namespace UnderscoreCs {
 	public static class _ {
 		public static void Each(IEnumerable<string> list, Action<string> iteratee) {
+			foreach (string item in list) {
+				iteratee(item);
+			}
 		}
 	}
 }
