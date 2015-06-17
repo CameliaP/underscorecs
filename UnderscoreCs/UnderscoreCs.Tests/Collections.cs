c39c99ffc8989f08842cf9c72953fd3fce758f91
Maxwell Bloch <mbloch@connectionseducation.com>
2015-06-17 17:12:58 -0400

    (RED) Each_GivenIEnumerable_AndIteratee_WhenCalled_ThenEachElementInListIsVisited

--- UnderscoreCs/UnderscoreCs.Tests/Collections.cs
+++ UnderscoreCs/UnderscoreCs.Tests/Collections.cs
@@ -1,7 +1,38 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace UnderscoreCs.Tests {
 	[TestClass]
 	public class Collections {
+
+		#region each
+
+		/// <summary>
+		/// http://underscorejs.org/#each
+		/// each	<code>_.each(list, iteratee, [context])</code>	Alias: forEach 
+		/// Iterates over a list of elements, yielding each in turn to an iteratee function.
+		/// The iteratee is bound to the context object, if one is passed. Each invocation of iteratee
+		/// is called with three arguments: <code>(element, index, list)</code>.
+		/// If list is a JavaScript object, iteratee's arguments will be <code>(value, key, list)</code>.
+		/// Returns the list for chaining.
+		/// </summary>
+		public string EachDoc;
+
+		[TestMethod]
+		public void Each_GivenIEnumerable_AndIteratee_WhenCalled_ThenEachElementInListIsVisited() {
+			IEnumerable<string> list = new[] {"A", "B", "C"};
+			Mock<IObserver<string>> mock = new Mock<IObserver<string>>(MockBehavior.Loose);
+			Action<string> iteratee = foo => { mock.Object.OnNext(foo); };
+
+			_.Each(list, iteratee);
+
+			mock.Verify(m => m.OnNext("A"));
+			mock.Verify(m => m.OnNext("B"));
+			mock.Verify(m => m.OnNext("C"));
+		}
+
+		#endregion
 	}
 }
--- UnderscoreCs/UnderscoreCs/_.cs
+++ UnderscoreCs/UnderscoreCs/_.cs
@@ -1,4 +1,9 @@
-﻿namespace UnderscoreCs {
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnderscoreCs {
 	public static class _ {
+		public static void Each(IEnumerable<string> list, Action<string> iteratee) {
+		}
 	}
 }
